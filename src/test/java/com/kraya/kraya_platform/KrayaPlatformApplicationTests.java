33ef8046fa37b82b65d3449afc76a370f8880e76
idrisSlimani <102354143+idrisSlimani@users.noreply.github.com>
2024-10-12 11:40:47 +0100

    Add Spring Boot project structure and springdoc-openapi dependency

---
+++ src/test/java/com/kraya/kraya_platform/KrayaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kraya.kraya_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrayaPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
